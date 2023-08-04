4ece6df2f66d1609ad6e7ec6f82c580bf6d17453
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-08-05 00:53:10 +0300

    guessnum | #6

--- Guessnum_js/static/js/guessnum.js
+++ Guessnum_js/static/js/guessnum.js
@@ -3,10 +3,12 @@ var difficultyIndicator = document.getElementById("difficulty_indicator");
 var playBody = document.getElementById("play_body");
 var display = document.getElementById("userNumberInput");
 var gameMessage = document.getElementById("game_message");
+var endGame = document.getElementById("end_game");
+var endGameMessage = document.getElementById("end_game_message");
 var minValueIndicator = document.getElementById("min_value");
 var maxValueIndicator = document.getElementById("max_value");
-var difficulty
-var calculation = "";
+var difficulty;
+var calculation;
 var minValue;
 var maxValue;
 var randomNumber;
@@ -31,6 +33,7 @@ function chooseDifficulty() {
   }
   randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
   console.log(randomNumber);
+  display.innerHTML = ""
   stepsCount = 0;
 
   maxValueIndicator.innerHTML = maxValue
@@ -39,6 +42,7 @@ function chooseDifficulty() {
 
 
   difficultyBlock.style.display = "none"; // Приховати блок
+  endGame.style.display = "none"; // Приховати блок
   difficultyIndicator.style.display = "block"; // Показати блок
   playBody.style.display = "block"; // Показати блок
 
@@ -66,19 +70,41 @@ function deleteLast() {
 function guessNumber() {
     stepsCount += 1;
     var userNumber = parseInt(display.innerHTML);
-    console.log(userNumber);
 
-    if (userNumber > randomNumber) {
-        gameMessage.innerHTML = "Ваше чило " + userNumber + " більше ніж треба. Спробуйте ще раз."
+    if (userNumber === randomNumber){
+        if (stepsCount === 1){
+        endGameMessage.innerHTML = "Ви вгадали число " + randomNumber + " з першої спроби! Це дуже круто! Вітаємо!";
+        }else if (stepsCount <= 4){
+        endGameMessage.innerHTML = "Ви вгадали число " + randomNumber + " за " + stepsCount + " спроби! Вітаємо!";
+        }else{
+        endGameMessage.innerHTML = "Ви вгадали число " + randomNumber + " за " + stepsCount + " спроб! Вітаємо!";
+        }
+        gameMessage.innerHTML = ""
+        playBody.style.display = "none"
+        endGame.style.display = "block";
+    }else if (userNumber > randomNumber) {
+        gameMessage.innerHTML = "Спроба " + stepsCount + ": Ваше число " + userNumber + " більше ніж треба. Спробуйте ще раз."
         display.innerHTML = "";
         maxValue = userNumber-1;
         maxValueIndicator.innerHTML = maxValue;
     }else if (userNumber < randomNumber) {
-        gameMessage.innerHTML = "Ваше чило " + userNumber + " менше ніж треба. Спробуйте ще раз."
+        gameMessage.innerHTML = "Спроба " + stepsCount + ": Ваше число " + userNumber + " менше ніж треба. Спробуйте ще раз."
         display.innerHTML = "";
         minValue = userNumber+1;
         minValueIndicator.innerHTML = minValue;
-    } else{
-    gameMessage.innerHTML = "Ви вгадали число " + randomNumber + " за " + stepsCount + " спроб! Вітаємо!"
+    }
+
+    if (minValue === maxValue){
+        playBody.style.display = "none"
+        endGame.style.display = "block";
+        gameMessage.innerHTML = ""
+        endGameMessage.innerHTML = "Залишився тільки один варіант " + randomNumber + ", тому, на жаль, Ви програли.";
     }
     }
+
+function new_game() {
+    endGame.style.display = "none";
+    endGameMessage.innerHTML = ""
+    difficultyBlock.style.display = "block";
+    difficultyIndicator.style.display = "none";
+}
