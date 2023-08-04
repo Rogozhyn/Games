987811d6d4a3f11ded23a55ae79084575673f628
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-08-04 16:33:43 +0300

    guessnum | #5

--- Guessnum_js/static/js/guessnum.js
+++ Guessnum_js/static/js/guessnum.js
@@ -2,8 +2,15 @@ var difficultyBlock = document.getElementById("choose_difficulty");
 var difficultyIndicator = document.getElementById("difficulty_indicator");
 var playBody = document.getElementById("play_body");
 var display = document.getElementById("userNumberInput");
+var gameMessage = document.getElementById("game_message");
+var minValueIndicator = document.getElementById("min_value");
+var maxValueIndicator = document.getElementById("max_value");
 var difficulty
 var calculation = "";
+var minValue;
+var maxValue;
+var randomNumber;
+var stepsCount;
 
 function chooseDifficulty() {
   let selectedDifficulty = document.querySelector('input[name="difficulty"]:checked');
@@ -14,7 +21,22 @@ function chooseDifficulty() {
   }
 
   difficulty = selectedDifficulty.value;
+  minValue = 0;
+  if (difficulty === "Легка") {
+    maxValue = 10;
+  } else if (difficulty === "Середня") {
+    maxValue = 100;
+  } else {
+    maxValue = 1000; //difficulty === "Складна"
+  }
+  randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+  console.log(randomNumber);
+  stepsCount = 0;
+
+  maxValueIndicator.innerHTML = maxValue
   difficultyIndicator.innerHTML = "Поточна складність гри: " + difficulty;
+  gameMessage.innerHTML = "Вкажіть ціле число від " + minValue + " до " + maxValue
+
 
   difficultyBlock.style.display = "none"; // Приховати блок
   difficultyIndicator.style.display = "block"; // Показати блок
@@ -24,112 +46,39 @@ function chooseDifficulty() {
 }
 
 function appendToDisplay(value) {
-  calculation = display.value + value;
-  display.value = calculation;
+    if (display.innerHTML.length == "0" && value == "0" ){
+        display.innerHTML = value;
+    } else if (display.innerHTML != "0" && parseInt(display.innerHTML + value) <= maxValue){
+        calculation = display.innerHTML + value;
+        display.innerHTML = calculation;
+    }
 }
 
 function clearDisplay() {
-    display.value = "";
+    display.innerHTML = "";
 }
 
 function deleteLast() {
-  calculation = display.value.slice(0, -1); // Видалити останній символ
-  display.value = calculation;
+  calculation = display.innerHTML.slice(0, -1); // Видалити останній символ
+  display.innerHTML = calculation;
 }
 
 function guessNumber() {
-  var gameResult = document.getElementById("gameResult");
+    stepsCount += 1;
+    var userNumber = parseInt(display.innerHTML);
+    console.log(userNumber);
 
-  var min_ = 0;
-  var max_;
-  
-  if (difficulty === "Легка") {
-    max_ = 10;
-  } else if (difficulty === "Середня") {
-    max_ = 100;
-  } else {
-    max_ = 1000;
-  }
-
-  var number = Math.floor(Math.random() * (max_ - min_ + 1) + min_);
-  console.log(number);
-
-  var messages = {
-    mes1: {
-      ua: `Залишився тільки один варіант ${number}, тому, на жаль, Ви програли.`,
-      en: `There is only one variant ${number} left, so, unfortunately, you lost.`,
-    },
-    mes2: {
-      ua: `Вкажіть ціле число від ${min_} до ${max_}: `,
-      en: `Enter an integer from ${min_} to ${max_}: `,
-    },
-    mes3: {
-      ua: `Треба вказувати лише цілі числа. Спробуйте ще раз.`,
-      en: `Should enter only integers. Try again.`,
-    },
-    mes4: {
-      ua: `Ваше чило {user_number} більше ніж треба. Спробуйте ще раз.`,
-      en: `Your number {user_number} is too large. Try again.`,
-    },
-    mes5: {
-      ua: `Ваше чило {user_number} менше ніж треба. Спробуйте ще раз.`,
-      en: `Your number {user_number} is less than required. Try again.`,
-    },
-    mes6: {
-      ua: `Ви вгадали число ${number} з першої спроби! Це дуже круто! Вітаємо!`,
-      en: `You guessed ${number} on the first try! It's very cool! Congratulations!`,
-    },
-    mes7: {
-      ua: `Ви вгадали число ${number} за {count} спроби! Вітаємо!`,
-      en: `You guessed ${number} in {count} tries! Congratulations!`,
-    },
-    mes8: {
-      ua: `Ви вгадали число ${number} за {count} спроб! Вітаємо!`,
-      en: `You guessed ${number} in {count} tries! Congratulations!`,
-    },
-    mes9: {
-      ua: "Для завершення натисніть Enter",
-      en: "Press Enter to finish",
-    },
-  };
-
-  var count = 0;
-  while (true) {
-    var user_number = parseInt(userNumberInput.value, 10);
-
-    if (isNaN(user_number)) {
-      console.log(messages.mes3[lang]);
-      continue;
+    if (userNumber > randomNumber) {
+        gameMessage.innerHTML = "Ваше чило " + userNumber + " більше ніж треба. Спробуйте ще раз."
+        display.innerHTML = "";
+        maxValue = userNumber-1;
+        maxValueIndicator.innerHTML = maxValue;
+    }else if (userNumber < randomNumber) {
+        gameMessage.innerHTML = "Ваше чило " + userNumber + " менше ніж треба. Спробуйте ще раз."
+        display.innerHTML = "";
+        minValue = userNumber+1;
+        minValueIndicator.innerHTML = minValue;
+    } else{
+    gameMessage.innerHTML = "Ви вгадали число " + randomNumber + " за " + stepsCount + " спроб! Вітаємо!"
     }
-
-    count += 1;
-    if (user_number > number) {
-      if (user_number - 1 === min_) {
-        console.log(messages.mes1[lang]);
-        break;
-      }
-      console.log(messages.mes4[lang]);
-      max_ = user_number - 1;
-    } else if (user_number < number) {
-      if (user_number + 1 === max_) {
-        console.log(messages.mes1[lang]);
-        break;
-      }
-      console.log(messages.mes5[lang]);
-      min_ = user_number + 1;
-    } else {
-      if (count === 1) {
-        console.log(messages.mes6[lang]);
-      } else if (count < 1 || count <= 4) {
-        console.log(messages.mes7[lang]);
-      } else {
-        console.log(messages.mes8[lang]);
-      }
-      break;
     }
-  }
-
-  gameResult.textContent = `${
-    "=========================================\n"
-  }${messages.mes9[lang]}`;
-}
